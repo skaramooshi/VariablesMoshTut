90b35b813d43b8a8330d4fd0c118b6b8d5d6b5b0
skara moosh <skaramoosh@rocketmail.com>
2020-04-21 19:06:30 +0400

    mosh tut

--- VariablesMoshTut/Program.cs
+++ VariablesMoshTut/Program.cs
@@ -6,8 +6,18 @@ namespace VariablesMoshTut
     {
         static void Main(string[] args)
         {
-            byte number;
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
